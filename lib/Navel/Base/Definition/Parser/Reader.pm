5b83958aee4acd46e13df00d02a622c8a6e8f436
le-garff-yoann <pe.weeble@yahoo.fr>
2015-09-02 17:12:02 +0200

    correct product name in # header

--- lib/Navel/Base/Definition/Parser/Reader.pm
+++ lib/Navel/Base/Definition/Parser/Reader.pm
@@ -1,5 +1,5 @@
 # Copyright 2015 Navel-IT
-# Navel Scheduler is developed by Yoann Le Garff, Nicolas Boquet and Yann Le Bras under GNU GPL v3
+# navel-base is developed by Yoann Le Garff, Nicolas Boquet and Yann Le Bras under GNU GPL v3
 
 #-> BEGIN
 
